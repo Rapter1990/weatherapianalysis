c39dd50edf7a7da2cf08c7225f6920650a52a7bb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-12 21:42:23 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/weatherapianalysis/WeatherApiAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherapianalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weatherapianalysis/WeatherApiAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherapianalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
