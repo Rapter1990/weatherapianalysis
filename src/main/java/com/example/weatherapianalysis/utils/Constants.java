d565bb619fcabef899d10ca086a6735cbc341d4e
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-13 17:39:03 +0300

    Task 8 : Revise Geocoding and AirPolution Url and Define feignclient dependency in pom.xml

--- src/main/java/com/example/weatherapianalysis/utils/Constants.java
+++ src/main/java/com/example/weatherapianalysis/utils/Constants.java
@@ -1,5 +1,6 @@
 package com.example.weatherapianalysis.utils;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,28 +14,27 @@ public class Constants {
     public static String OPEN_WEATHER_AIR_POLLUTION_URL;
 
     @Value("${open-weather-api.api-url}")
-    public void setOpenWeatherApiBaseUrl(String apiUrl) {
-        OPEN_WEATHER_API_BASE_URL = apiUrl;
-    }
+    private String apiUrlProperty;
 
     @Value("${open-weather-api.api-key}")
-    public void setOpenWeatherApiKey(String apiKey) {
-        OPEN_WEATHER_API_KEY = apiKey;
-    }
+    private String apiKeyProperty;
 
-    @Value("${exchange-api.api-call-limit}")
-    public void setOpenWeatherApiCallLimit(Integer apiCallLimit) {
-        OPEN_WEATHER_API_CALL_LIMIT = apiCallLimit;
-    }
+    @Value("${open-weather-api.api-call-limit}")
+    private Integer apiCallLimitProperty;
 
-    @Value("${open-weather-api.api-url}")
-    public void setGeocodingUrl(String apiUrl) {
-        OPEN_WEATHER_GEOCODING_URL = apiUrl + "/geo/1.0/direct";
-    }
+    @Value("${open-weather-api.geocode-url}")
+    private String geocodingUrlProperty;
 
-    @Value("${open-weather-api.api-url}")
-    public void setAirPollutionUrl(String apiUrl) {
-        OPEN_WEATHER_AIR_POLLUTION_URL = apiUrl + "/data/2.5/air_pollution/history";
-    }
+    @Value("${open-weather-api.air-pollution-url}")
+    private String airPollutionUrlProperty;
 
+    @PostConstruct
+    public void init() {
+        OPEN_WEATHER_API_BASE_URL = apiUrlProperty;
+        OPEN_WEATHER_API_KEY = apiKeyProperty;
+        OPEN_WEATHER_API_CALL_LIMIT = apiCallLimitProperty;
+        OPEN_WEATHER_GEOCODING_URL = geocodingUrlProperty;
+        OPEN_WEATHER_AIR_POLLUTION_URL = airPollutionUrlProperty;
+    }
 }
+
