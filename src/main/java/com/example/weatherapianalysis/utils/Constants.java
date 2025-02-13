55b447c415cfea7d46c8ee56ee410068c82520c6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-13 10:26:33 +0300

    Task 6 : Add GeocodingUrl and AirPollutionUrl in Constants

--- src/main/java/com/example/weatherapianalysis/utils/Constants.java
+++ src/main/java/com/example/weatherapianalysis/utils/Constants.java
@@ -8,8 +8,9 @@ public class Constants {
 
     public static String OPEN_WEATHER_API_BASE_URL;
     public static String OPEN_WEATHER_API_KEY;
-
     public static Integer OPEN_WEATHER_API_CALL_LIMIT;
+    public static String OPEN_WEATHER_GEOCODING_URL;
+    public static String OPEN_WEATHER_AIR_POLLUTION_URL;
 
     @Value("${open-weather-api.api-url}")
     public void setOpenWeatherApiBaseUrl(String apiUrl) {
@@ -24,6 +25,16 @@ public class Constants {
     @Value("${exchange-api.api-call-limit}")
     public void setOpenWeatherApiCallLimit(Integer apiCallLimit) {
         OPEN_WEATHER_API_CALL_LIMIT = apiCallLimit;
+    }
+
+    @Value("${open-weather-api.api-url}")
+    public void setGeocodingUrl(String apiUrl) {
+        OPEN_WEATHER_GEOCODING_URL = apiUrl + "/geo/1.0/direct";
+    }
+
+    @Value("${open-weather-api.api-url}")
+    public void setAirPollutionUrl(String apiUrl) {
+        OPEN_WEATHER_AIR_POLLUTION_URL = apiUrl + "/data/2.5/air_pollution/history";
     }
 
 }
