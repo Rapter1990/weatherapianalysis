f5572273e117dcdb280a2e4f0ba05e07d3dd0b6c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-13 17:58:36 +0300

    Task 11 : Revise all imports

--- src/main/java/com/example/weatherapianalysis/model/dto/response/OpenWeatherResponse.java
+++ src/main/java/com/example/weatherapianalysis/model/dto/response/OpenWeatherResponse.java
@@ -2,6 +2,7 @@ package com.example.weatherapianalysis.model.dto.response;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
 
 @Getter
--- src/main/java/com/example/weatherapianalysis/service/client/OpenWeatherClient.java
+++ src/main/java/com/example/weatherapianalysis/service/client/OpenWeatherClient.java
@@ -1,7 +1,6 @@
 package com.example.weatherapianalysis.service.client;
 
 import com.example.weatherapianalysis.model.dto.response.OpenWeatherResponse;
-import com.example.weatherapianalysis.utils.Constants;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
