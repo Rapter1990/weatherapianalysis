1692e6e9e42b1407ccccb9423ebce2256c718ce9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-15 19:37:21 +0300

    Task 20 : Define BaseEntity

---
+++ src/main/java/com/example/weatherapianalysis/model/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.example.weatherapianalysis.model.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
