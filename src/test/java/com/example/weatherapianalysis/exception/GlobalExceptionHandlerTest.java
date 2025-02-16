afd8407b9441145fd0e3510621a827fbdb1881fb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-16 14:42:22 +0300

    Task 30 : Define JUnit tests in GlobalExceptionHandlerTest

---
+++ src/test/java/com/example/weatherapianalysis/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,182 @@
+package com.example.weatherapianalysis.exception;
+
+import com.example.weatherapianalysis.base.AbstractRestControllerTest;
+import com.example.weatherapianalysis.exception.error.CustomError;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class GlobalExceptionHandlerTest extends AbstractRestControllerTest {
+
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Test
+    void givenMethodArgumentNotValidException_whenHandleMethodArgumentNotValid_thenRespondWithBadRequest() {
+
+        // Given
+        BindingResult bindingResult = mock(BindingResult.class);
+        MethodArgumentNotValidException ex = new MethodArgumentNotValidException(null, bindingResult);
+        FieldError fieldError = new FieldError("objectName", "fieldName", "error message");
+        List<ObjectError> objectErrors = Collections.singletonList(fieldError);
+
+        when(bindingResult.getAllErrors()).thenReturn(objectErrors);
+
+        CustomError expectedError = CustomError.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .header(CustomError.Header.VALIDATION_ERROR.getName())
+                .message("Validation failed")
+                .subErrors(Collections.singletonList(
+                        CustomError.CustomSubError.builder()
+                                .field("fieldName")
+                                .message("error message")
+                                .build()))
+                .build();
+
+        // When
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentNotValid(ex);
+
+        // Then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        CustomError actualError = (CustomError) responseEntity.getBody();
+        checkCustomError(expectedError, actualError);
+
+    }
+
+    @Test
+    void givenObjectError_whenHandleMethodArgumentNotValid_thenRespondWithBadRequest() {
+
+        // Given
+        BindingResult bindingResult = mock(BindingResult.class);
+        MethodArgumentNotValidException ex = new MethodArgumentNotValidException(null, bindingResult);
+        ObjectError objectError = new ObjectError("objectName", "object level error message");
+        List<ObjectError> objectErrors = Collections.singletonList(objectError);
+
+        when(bindingResult.getAllErrors()).thenReturn(objectErrors);
+
+        CustomError expectedError = CustomError.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .header(CustomError.Header.VALIDATION_ERROR.getName())
+                .message("Validation failed")
+                .subErrors(Collections.singletonList(
+                        CustomError.CustomSubError.builder()
+                                .field("objectName")
+                                .message("object level error message")
+                                .build()
+                ))
+                .build();
+
+        // When
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentNotValid(ex);
+
+        // Then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        CustomError actualError = (CustomError) responseEntity.getBody();
+        checkCustomError(expectedError, actualError);
+    }
+
+    @Test
+    void givenConstraintViolationException_whenHandlePathVariableErrors_thenRespondWithBadRequest() {
+
+        // Given
+        ConstraintViolation<String> mockViolation = mock(ConstraintViolation.class);
+        Path mockPath = mock(Path.class);
+        Set<ConstraintViolation<?>> violations = Set.of(mockViolation);
+        ConstraintViolationException mockException = new ConstraintViolationException(violations);
+
+        CustomError.CustomSubError subError = CustomError.CustomSubError.builder()
+                .message("must not be null")
+                .field("")
+                .value("invalid value")
+                .type("String") // Default to String if getRootBeanClass() is null
+                .build();
+
+        CustomError expectedError = CustomError.builder()
+                .time(LocalDateTime.now())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .header(CustomError.Header.VALIDATION_ERROR.getName())
+                .message("Constraint violation")
+                .subErrors(Collections.singletonList(subError))
+                .build();
+
+        // When
+        when(mockViolation.getMessage()).thenReturn("must not be null");
+        when(mockViolation.getPropertyPath()).thenReturn(mockPath);
+        when(mockPath.toString()).thenReturn("field");
+        when(mockViolation.getInvalidValue()).thenReturn("invalid value");
+        when(mockViolation.getRootBeanClass()).thenReturn(String.class); // Ensure this does not return null
+
+        // Then
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handlePathVariableErrors(mockException);
+
+        CustomError actualError = (CustomError) responseEntity.getBody();
+
+        // Verify
+        checkCustomError(expectedError, actualError);
+
+    }
+
+    @Test
+    void givenRuntimeException_whenHandleRuntimeException_thenRespondWithNotFound() {
+        // Given
+        RuntimeException ex = new RuntimeException("Resource not found");
+
+        CustomError expectedError = CustomError.builder()
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .header(CustomError.Header.API_ERROR.getName())
+                .message("Resource not found")
+                .build();
+
+        // When
+        ResponseEntity<?> responseEntity = globalExceptionHandler.handleRuntimeException(ex);
+
+        // Then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        CustomError actualError = (CustomError) responseEntity.getBody();
+        checkCustomError(expectedError, actualError);
+    }
+
+
+    private void checkCustomError(CustomError expectedError, CustomError actualError) {
+
+        assertThat(actualError).isNotNull();
+        assertThat(actualError.getTime()).isNotNull();
+        assertThat(actualError.getHeader()).isEqualTo(expectedError.getHeader());
+        assertThat(actualError.getIsSuccess()).isEqualTo(expectedError.getIsSuccess());
+
+        if (expectedError.getMessage() != null) {
+            assertThat(actualError.getMessage()).isEqualTo(expectedError.getMessage());
+        }
+
+        if (expectedError.getSubErrors() != null) {
+            assertThat(actualError.getSubErrors().size()).isEqualTo(expectedError.getSubErrors().size());
+            if (!expectedError.getSubErrors().isEmpty()) {
+                assertThat(actualError.getSubErrors().get(0).getMessage()).isEqualTo(expectedError.getSubErrors().get(0).getMessage());
+                assertThat(actualError.getSubErrors().get(0).getField()).isEqualTo(expectedError.getSubErrors().get(0).getField());
+                assertThat(actualError.getSubErrors().get(0).getValue()).isEqualTo(expectedError.getSubErrors().get(0).getValue());
+                assertThat(actualError.getSubErrors().get(0).getType()).isEqualTo(expectedError.getSubErrors().get(0).getType());
+            }
+        }
+
+    }
+
+}
